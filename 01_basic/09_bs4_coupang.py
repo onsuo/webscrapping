6f83494b6be954dffac7c7800bb12758a3b680f9
ouuno <jaewon.rhea@gmail.com>
2022-08-01 16:28:23 +0900

    coupang error

--- 01_basic/09_bs4_coupang.py
+++ 01_basic/09_bs4_coupang.py
@@ -2,14 +2,13 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=recent&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=6&backgroundColor="
-headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15"}
+url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=5&backgroundColor="
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
 res = requests.get(url, headers=headers)
 res.raise_for_status()
-# soup = BeautifulSoup(res.text, "lxml")
+soup = BeautifulSoup(res.text, "lxml")
 
-with open("coupang.html", "w", encoding="utf8") as f:
-    f.write(res.text)
+print(res.text)
 
 # items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
 # print(items[0].find("div", attrs={"class":"name"}).get_text())
